7ff510df896eee16a5b64689456f869b29eb7e01
Wangxu18 <wangxiaoyong1993@126.com>
2023-02-09 11:16:41 +0800

    add 230208_gd 230208_gd_C

---
+++ qus_test_solution/230208_gd/230208_gd.cpp
@@ -0,0 +1,101 @@
+﻿// 230208_gd.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <windows.h>
+#include <strsafe.h>
+#include <conio.h>
+
+#define BUFSIZE 4096
+
+VOID GetAnswerToRequest(LPTSTR chRequest, LPTSTR chReply, LPDWORD pchBytes) {
+  _tprintf(TEXT("%s\n"), chRequest);
+  StringCchCopy(chReply, BUFSIZE, TEXT("Default answer from server"));
+  *pchBytes = (lstrlen(chReply) + 1) * sizeof(TCHAR);
+}
+
+DWORD WINAPI InstanceThread(LPVOID lpvParam) {
+  TCHAR chRequest[BUFSIZE];
+  TCHAR chReply[BUFSIZE];
+  DWORD cbBytesRead, cbReplyBytes, cbWritten;
+  BOOL fSuccess;
+  HANDLE hPipe;
+
+  // The thread's parameter is a handle to a pipe instance.   
+
+  hPipe = (HANDLE)lpvParam;
+
+  while (1)
+  {
+	// Read client requests from the pipe.   
+	fSuccess = ReadFile(
+	  hPipe,        // handle to pipe   
+	  chRequest,    // buffer to receive data   
+	  BUFSIZE * sizeof(TCHAR), // size of buffer   
+	  &cbBytesRead, // number of bytes read   
+	  NULL);        // not overlapped I/O   
+
+	if (!fSuccess || cbBytesRead == 0)
+	  break;
+	//printf((const char*)chRequest);
+	std::cout << *chRequest << std::endl;
+
+	GetAnswerToRequest(chRequest, chReply, &cbReplyBytes);
+
+	// Write the reply to the pipe.   
+	fSuccess = WriteFile(
+	  hPipe,        // handle to pipe   
+	  chReply,      // buffer to write from   
+	  cbReplyBytes, // number of bytes to write   
+	  &cbWritten,   // number of bytes written   
+	  NULL);        // not overlapped I/O   
+
+	if (!fSuccess || cbReplyBytes != cbWritten) break;
+  }
+
+  // Flush the pipe to allow the client to read the pipe's contents   
+  // before disconnecting. Then disconnect the pipe, and close the   
+  // handle to this pipe instance.   
+
+  FlushFileBuffers(hPipe);
+  DisconnectNamedPipe(hPipe);
+  CloseHandle(hPipe);
+
+  return 1;
+}
+
+
+int main()
+{
+  BOOL fConnected;
+  DWORD dwThreadId;
+  HANDLE hPipe, hThread;
+  LPCWSTR lpPipeName = TEXT("\\\\.\\pipe\\mynamepipe");
+
+  while (1)
+  {
+	hPipe = CreateNamedPipe(lpPipeName, PIPE_ACCESS_DUPLEX,
+							PIPE_TYPE_MESSAGE|PIPE_READMODE_MESSAGE|PIPE_WAIT,
+							PIPE_UNLIMITED_INSTANCES, BUFSIZE, BUFSIZE, 0, NULL);
+
+	if (hPipe == INVALID_HANDLE_VALUE) {
+	  std::cout << "create pipe failed" << std::endl;
+	  return 0;
+	}
+
+	fConnected = ConnectNamedPipe(hPipe, NULL) ? TRUE : (GetLastError() == ERROR_PIPE_CONNECTED);
+	if (fConnected) {
+	  hThread = CreateThread(NULL, 0, InstanceThread, (LPVOID)hPipe, 0, &dwThreadId);
+	  if (hThread == NULL) {
+		std::cout << "create thread failed" << std::endl;
+		return 0;
+	  }
+	  else {
+		::CloseHandle(hThread);
+	  }
+	} else {
+	  ::CloseHandle(hPipe);
+	}
+  }
+}
+
---
+++ qus_test_solution/230208_gd_C/230208_gd_C.cpp
@@ -0,0 +1,108 @@
+﻿// 230208_gd_C.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <windows.h>
+
+#define BUFSIZE 4096
+
+int main()
+{
+  HANDLE hPipe;
+  LPCWSTR lpvMessage = TEXT("Default message from client");
+  TCHAR chBuf[BUFSIZE];
+  BOOL fSuccess;
+  DWORD cbRead, cbWritten, dwMode;
+  LPCWSTR lpszPipename = TEXT("\\\\.\\pipe\\mynamepipe");
+
+  /*if (argc > 1)
+	lpvMessage = argv[1];*/
+
+  // Try to open a named pipe; wait for it, if necessary.   
+
+  while (1)
+  {
+	hPipe = CreateFile(
+	  lpszPipename,   // pipe name   
+	  GENERIC_READ |  // read and write access   
+	  GENERIC_WRITE,
+	  0,              // no sharing   
+	  NULL,           // default security attributes  
+	  OPEN_EXISTING,  // opens existing pipe   
+	  0,              // default attributes   
+	  NULL);          // no template file   
+
+  // Break if the pipe handle is valid.   
+
+	if (hPipe != INVALID_HANDLE_VALUE)
+	  break;
+
+	// Exit if an error other than ERROR_PIPE_BUSY occurs.   
+
+	if (GetLastError() != ERROR_PIPE_BUSY)
+	{
+	  printf("Could not open pipe");
+	  return 0;
+	}
+
+	// All pipe instances are busy, so wait for 20 seconds.   
+
+	if (!WaitNamedPipe(lpszPipename, 20000))
+	{
+	  printf("Could not open pipe");
+	  return 0;
+	}
+  }
+
+  // The pipe connected; change to message-read mode.   
+
+  dwMode = PIPE_READMODE_MESSAGE;
+  fSuccess = SetNamedPipeHandleState(
+	hPipe,    // pipe handle   
+	&dwMode,  // new pipe mode   
+	NULL,     // don't set maximum bytes   
+	NULL);    // don't set maximum time   
+  if (!fSuccess)
+  {
+	printf("SetNamedPipeHandleState failed");
+	return 0;
+  }
+
+  // Send a message to the pipe server.   
+
+  fSuccess = WriteFile(
+	hPipe,                  // pipe handle   
+	lpvMessage,             // message   
+	(lstrlen(lpvMessage) + 1) * sizeof(TCHAR), // message length   
+	&cbWritten,             // bytes written   
+	NULL);                  // not overlapped   
+  if (!fSuccess)
+  {
+	printf("WriteFile failed");
+	return 0;
+  }
+
+  do
+  {
+	// Read from the pipe.   
+
+	fSuccess = ReadFile(
+	  hPipe,    // pipe handle   
+	  chBuf,    // buffer to receive reply   
+	  BUFSIZE * sizeof(TCHAR),  // size of buffer   
+	  &cbRead,  // number of bytes read   
+	  NULL);    // not overlapped   
+
+	if (!fSuccess && GetLastError() != ERROR_MORE_DATA)
+	  break;
+
+	//_tprintf(TEXT("%s\n"), chBuf);
+  } while (!fSuccess);  // repeat loop if ERROR_MORE_DATA   
+
+  //getch();
+
+  CloseHandle(hPipe);
+
+  return 0;
+}
+
